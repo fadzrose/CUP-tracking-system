3bd6f2724e32fea600e741bae3eb82b3d956b17c
fadzrose <fadzillahroslan5903@gmail.com>
2023-12-14 12:05:09 +0800

    sort

--- reportbyCategory.php
+++ reportbyCategory.php
@@ -163,6 +163,7 @@
             transition: margin-left .5s;
             /* If you want a transition effect */
             padding: 20px;
+            padding-bottom: 80px;
         }
 
         /* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
@@ -175,6 +176,67 @@
                 font-size: 18px;
             }
         }
+
+        /* Style the dropdown button */
+        .dropbtn {
+            background-color: #ff8fab;
+            color: white;
+            padding: 12px;
+            font-size: 16px;
+            border: none;
+            cursor: pointer;
+            border-radius: 4px;
+            width: 200px;
+
+        }
+
+        /* Dropdown button on hover & focus */
+        .dropbtn:hover,
+        .dropbtn:focus {
+            background-color: pink;
+        }
+
+        /* Style the dropdown content */
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+            border-radius: 4px;
+        }
+
+        /* Links inside the dropdown */
+        .dropdown-content a {
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            border-radius: 4px;
+        }
+
+        /* Change color of dropdown links on hover */
+        .dropdown-content a:hover {
+            background-color: #f1f1f1;
+            border-radius: 4px;
+        }
+
+        /* Show the dropdown menu on hover */
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
+
+        /* Show the dropdown menu when the button is focused */
+        .dropdown:focus-within .dropdown-content {
+            display: block;
+        }
+
+        .title {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
     </style>
 </head>
 
@@ -208,97 +270,117 @@
     </div>
     <br>
     <div id="main" class="card">
-
-        <h2>Collapsed Sidebar</h2>
-
-        <div>
-            <div class="card-header py-3">
-                <div class="form-row justify-content-between align-items-center">
-                    <h5 class="m-0 font-weight-bold text-dark">Details of Projects</h5>
-                    <ul class="nav-item dropdown no-arrow mx-1">
-                        <a href="#" class="btn btn-secondary btn-icon-split nav-link dropdown-toggle ml-auto" id="sortingDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="icon text-white-50">
-                                <i class="fas fa-caret-down"></i>
-                            </span>
-                            <span class="text">Sort by Project</span>
-                        </a>
-
-                        <!-- Dropdown - Sorting -->
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="sortingDropdown">
-                            <a class="dropdown-item" href="allprojectlist.php">
-                                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                sort by Project
-                            </a>
-                            <a class="dropdown-item" href="sortprogresslist.php">
-                                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                sort by Progress
-                            </a>
-                            <a class="dropdown-item" href="sorteditorlist.php">
-                                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                sort by Editor
-                            </a>
-                            <a class="dropdown-item" href="sortdesignerlist.php">
-                                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                sort by Graphic Designer
-                            </a>
-                            <a class="dropdown-item" href="sortillustratorlist.php">
-                                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                sort by Illustrator
-                            </a>
+        <div class="title">
+            <h2 align="left">Report</h2>
+            <div class="dropdown" align="right">
 
 
-                        </div>
-                    </ul>
+                <button onclick="toggleDropdown()" class="dropbtn">Sort by Category</button>
+                <div id="dropdownContent" class="dropdown-content">
+
+                    <a href="reportbyCategory.php">sort by Category</a>
+                    <a href="#">sort by Progress</a>
+                    <a href="#">sort by Editor</a>
+                    <a href="#">sort by Graphic Designer</a>
+                    <a href="#">sort by Illustrator</a>
                 </div>
             </div>
+        </div>
+        <br>
+
+        <div>
+
+
             <div class="card-body">
                 <div class="table-responsive">
                     <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                         <thead>
-                            <tr>
+                            <tr class="head-color">
                                 <th>Title</th>
                                 <th>Series</th>
-                                <th>Category</th>
+
                                 <th>Size</th>
                                 <th>Total Pages</th>
                                 <th>Type of Design</th>
                                 <th>Finishing</th>
-                                <th>Action</th>
+
 
                             </tr>
                         </thead>
+                        <?php
+                        include "dbconnect.php";
+
+                        // Modify the SQL query to order the results by category
+                        $sql = "SELECT project.*, design_layout.* 
+        FROM project 
+        INNER JOIN design_layout ON project.projectId = design_layout.projectId 
+        ORDER BY category"; // Order by the 'category' column
+
+                        $result = mysqli_query($dbc, $sql);
+
+                        $currentCategory = null; // Variable to keep track of the current category
+
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            // Check if the current row's category is different from the previous row's category
+                            if ($row['category'] !== $currentCategory) {
+                                // If it's a new category, display the category as the header
+                                echo '<tr><th colspan="7">' . $row['category'] . '</th></tr>';
+                                $currentCategory = $row['category']; // Update the current category
+                            }
+
+                            // Display the data excluding the category column
+                            echo '<tr>
+            <td>' . $row['title'] . '</td>
+            <td>' . $row['siri'] . '</td>
+            <td>' . $row['projectSize'] . '</td>
+            <td>' . $row['totalPages'] . '</td>
+            <td>' . $row['typeOfDesign'] . '</td>
+            <td>' . $row['typeOfFinishing'] . '</td>
+          </tr>';
+                        }
+                        ?>
+
                     </table>
                 </div>
             </div>
+
         </div>
-    </div>
 
-    <div id="footer" class="footer">
-        <br>
-        <p>Copyright &copy; 2023 Ana Muslim Sdn Bhd. All rightsreserved.</p>
+        <div id="footer" class="footer">
+            <br>
+            <p>Copyright &copy; 2023 Ana Muslim Sdn Bhd. All rightsreserved.</p>
 
-    </div>
+        </div>
 
-    <script>
-        /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-        function openNav() {
-            document.getElementById("mySidebar").style.width = "200px";
-            document.getElementById("main").style.marginLeft = "240px";
-            document.getElementById("main").style.marginRight = "40px";
-            document.getElementById("footer").style.marginLeft = "200px";
-            document.getElementById("header").style.marginLeft = "200px";
-        }
+        <script>
+            /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+            function openNav() {
+                document.getElementById("mySidebar").style.width = "200px";
+                document.getElementById("main").style.marginLeft = "240px";
+                document.getElementById("main").style.marginRight = "40px";
+                document.getElementById("footer").style.marginLeft = "200px";
+                document.getElementById("header").style.marginLeft = "200px";
+            }
 
-        /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-        function closeNav() {
-            document.getElementById("mySidebar").style.width = "0";
-            document.getElementById("main").style.marginLeft = "5%";
-            document.getElementById("footer").style.marginLeft = "0";
-            document.getElementById("header").style.marginLeft = "0";
-        }
-    </script>
+            /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+            function closeNav() {
+                document.getElementById("mySidebar").style.width = "0";
+                document.getElementById("main").style.marginLeft = "5%";
+                document.getElementById("footer").style.marginLeft = "0";
+                document.getElementById("header").style.marginLeft = "0";
+            }
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+            function toggleDropdown() {
+                var dropdownContent = document.getElementById("dropdownContent");
+                if (dropdownContent.style.display === "block") {
+                    dropdownContent.style.display = "none";
+                } else {
+                    dropdownContent.style.display = "block";
+                }
+            }
+        </script>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </body>
 
 </html>
